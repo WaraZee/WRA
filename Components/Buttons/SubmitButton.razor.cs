2ed5c7ef4c1c1df52b940cf5bf8df9d915b3261e
WaraZee <moto_vr_46@yahoo.co.jp>
2024-01-31 00:08:27 +0900

    added Loader.razor

---
+++ Components/Buttons/SubmitButton.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WRA.Components.Buttons
+{
+    public partial class SubmitButton
+    {
+        [Parameter]
+        public string Course { get; set; } = "";
+
+        protected override void OnInitialized()
+        {
+            StateContainer.OnChange += StateHasChanged;
+        }
+
+        private void ChangePropertyValue(bool isLoading)
+        {
+            StateContainer.IsLoading = !isLoading;
+        }
+
+        public void Dispose()
+        {
+            StateContainer.OnChange -= StateHasChanged;
+        }
+
+        private async void OnClickAsync()
+        {
+            ChangePropertyValue(StateContainer.IsLoading);
+            await OnClick.InvokeAsync();
+            ChangePropertyValue(StateContainer.IsLoading);
+        }
+    }
+}
